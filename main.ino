1db6cde771f9d11007ea3fb969252d05e515690b
KevinGQueensU <127079352+KevinGQueensU@users.noreply.github.com>
2025-11-29 20:10:25 -0500

    added print command for debugging

--- main.ino
+++ main.ino
@@ -1,7 +1,7 @@
 #include <RTC.h>
 #include <Dispenser.h>
 
-int IR_PIN = 4;
+int IR_PIN = 1;
 IRSensor irs(IR_PIN);
 
 int RTC_PIN = 2;
@@ -12,11 +12,16 @@ byte yyyy = temp.year();
 byte hr = temp.hour();
 byte min = temp.minute();
 byte sec = temp.second();
-byte DOW = 4;
-RTC rtc(RTC_PIN, mm, dd, yyyy, hr, min, sec, DOW);
 
-int DOUT_pin = 6;
-int SCK_pin = 5;
+uint32_t epoch = temp.unixtime();        // seconds since 1970-01-01
+uint32_t days  = epoch / 86400UL;        // whole days since epoch
+byte DOW_0_6 = (days + 4) % 7;           // 0=Sunday, 1=Monday, ... 6=Saturday
+byte DOW = DOW_0_6 + 1;
+
+RTC rtc(RTC_PIN, mm, dd, yyyy, hr, min, sec, DOW_0_6);
+
+int DOUT_pin = 5;
+int SCK_pin = 6;
 LoadCell scale(DOUT_pin, SCK_pin);
 
 int RX_pin = 10;
@@ -31,12 +36,12 @@ void setup() {
     Serial.begin(115200);
     sys.begin(&tick);
     scale.set_calibration(17131.14);
+    Serial.println("Setup Complete");
     delay(10);
 }
 
 void loop() {
     if (Serial.available() > 0) {
-        // read header
         String header = Serial.readStringUntil('\n');
         header.trim();
         if(header == "DUMP"){
@@ -46,32 +51,36 @@ void loop() {
             sys.ClearAlarms();
         }
         else if (header == "PRINT") {
+            Serial.print("Current RTC: ");
+            Serial.print(rtc.get_DOW_str());
+            Serial.print(' ');
+            Serial.println(rtc.get_time_str());
             Serial.println("Alarms:");
-            sys.PrintAlarms();           // <-- only prints when you send PRINT
+            sys.PrintAlarms();          
         }
-        else if(header == "BEGIN"){
-            // read DOW and time
+        else if (header == "BEGIN") {
             String DOW_read  = Serial.readStringUntil('\n');
-
             String time_read = Serial.readStringUntil('\n');
-            // read 12 ints
+
+            DOW_read.trim();       // remove \r / spaces
+            time_read.trim();    
+
+
             int pills[12];
             for (int i = 0; i < 12; i++) {
-                pills[i] = Serial.parseInt();
-                Serial.readStringUntil('\n');
+                String line = Serial.readStringUntil('\n');
+                line.trim();
+                pills[i] = line.toInt();
             }
             Serial.readStringUntil('\n');
-
-            // check for end line
+            
             String endLine = Serial.readStringUntil('\n');
             endLine.trim();
 
+
             if (endLine == "END") {
                 sys.AddAlarm(time_read, DOW_read, pills, 1);
-                Serial.println("Alarm added");
             } else {
-                Serial.println("Bad packet (missing END), ignored");
-                // flush whatever is left so we resync cleanly
                 while (Serial.available() > 0) Serial.read();
             }
         }
@@ -79,7 +88,7 @@ void loop() {
     
     if (tick) {
         tick = 0;
-        sys.Dispense(0);
+        sys.Dispense(1);
         while(!tick){
             delay(10);
         }
